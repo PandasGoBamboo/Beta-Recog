767adf008101917a92a8cc075dbbe547255b5162
PandasGoBamboo <t.schumacher91@gmx.de>
2022-09-19 10:09:54 +0200

    dsad

--- new.py
+++ new.py
@@ -18,29 +18,44 @@ root = tree.getroot()
 
 
 pforms = []
-# sucht und printet Präsentationsform
+# sucht Präsentationsform und fügt sie zur Liste hinzu
 for pform in root.iter('PRAESENTATIONSFORM'):
-    #print(pform.text)
     pforms.append(pform.text)
-    #df['pform'] = pform.text
 
-titels = []
-# sucht und printent Text unter allen TITEL.Tags, also Hauptitel, Sonstiger Titel, Seitentitel
-for titel in root.findall("./INHALT/TITEL/"):
-    #print(titel.text)
-    titels.append(titel.text)
-    #df['titel'] = titel.text
+haupt_titels = []
+# sucht Haupttitel und fügt sie ihn Liste hinzu
+for haupt_titel in root.findall("./INHALT/TITEL/HAUPTTITEL"):
+    haupt_titels.append(haupt_titel.text)
+
+sonst_titels = []
+# sucht sonstige Titel und fügt ihn zur Liste hinzu
+for sonst_titel in root.findall("./INHALT/TITEL/SONSTIGER_TITEL"):
+    sonst_titels.append(sonst_titel.text)
+
+seiten_titels = []
+# sucht Seitentitel und fügt ihn zur Liste hinzu
+for seiten_titel in root.findall("./INHALT/TITEL/SEITENTITEL"):
+    seiten_titels.append(seiten_titel.text)
 
 texts = []
-# sucht und printent Text unter VOLLTEXT.Tags
-for text in root.findall("./INHALT/VOLLTEXT/"):
-    #print(text.text)
+# such Volltext-Text und fügt ihn zur Liste hinzu
+for text in root.findall("./INHALT/VOLLTEXT/TEXT"):
     texts.append(text.text)
-    #df['text'] = text.text
 
-print(pforms)
-print(titels)
-print(texts)
+train_data = pd.DataFrame(
+    {'pform': pforms,
+     'haupt_titel': haupt_titels,
+     'sonst_titel': sonst_titels,
+     'seiten_titel': seiten_titels,
+     'volltext': texts,
+    })
+print(train_data.head)
+
+#print(pforms)
+#print(haupt_titels)
+#print(sonst_titels)
+#print(seiten_titels)
+#print(texts)
 
 """
 # root[2] = Inhalt
